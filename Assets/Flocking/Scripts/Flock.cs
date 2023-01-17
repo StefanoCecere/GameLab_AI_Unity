4638d31908e747c16e6fe06cf5e15e0ddd91520e
Stefano Cecere <stefano.cecere@krur.com>
2023-01-17 23:17:34 +0100

    flocking namespace

--- Assets/Flocking/Scripts/Flock.cs
+++ Assets/Flocking/Scripts/Flock.cs
@@ -1,115 +1,134 @@
 ﻿using UnityEngine;
 
-public class Flock : MonoBehaviour
+namespace flocking
 {
-
-    // Access the FlockManager script
-    public FlockManager myManager;
-    // Prefab initial speed;
-    float speed;
-    // Bool used to check the swim limits
-    bool turning = false;
-
-    void Start()
-    {
-        // Assign a random speed to each this prefab
-        speed = Random.Range(myManager.minSpeed, myManager.maxSpeed);
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        // Determine the bounding box of the manager cube
-        Bounds b = new Bounds(myManager.transform.position, myManager.swimLimits * 2.0f);
-
-        // If the fish is outside the bounds of the cube or about to hit something
-        // then start turning around
-        RaycastHit hit = new RaycastHit();
-        Vector3 direction = Vector3.zero;
-
-        if (!b.Contains(transform.position)) {
-
-            turning = true;
-            direction = myManager.transform.position - transform.position;
-        } else if (Physics.Raycast(transform.position, this.transform.forward * 50.0f, out hit)) {
-            turning = true;
-            //Debug.DrawRay(this.transform.position, this.transform.forward * 50.0f, Color.red);
-            direction = Vector3.Reflect(this.transform.forward, hit.normal);
-        } else {
-
-            turning = false;
-        }
-
-        // Test if we're turning
-        if (turning) {
-
-            // Turn towards the centre of the cube
-            transform.rotation = Quaternion.Slerp(transform.rotation,
-                                                  Quaternion.LookRotation(direction),
-                                                  myManager.rotationSpeed * Time.deltaTime);
-        } else {
-
-            // 10% chance of altering prefab speed
-            if (Random.Range(0.0f, 100.0f) < 10.0f) {
-
-                speed = Random.Range(myManager.minSpeed, myManager.maxSpeed);
-            }
-
-            // 20& chance of applying the flocking rules
-            if (Random.Range(0.0f, 100.0f) < 20.0f) {
-
-                ApplyRules();
-            }
-        }
-
-        transform.Translate(0.0f, 0.0f, Time.deltaTime * speed);
-    }
-
-    void ApplyRules()
+    public class Flock : MonoBehaviour
     {
 
-        GameObject[] gos;
-        gos = myManager.allFish;
+        // Access the FlockManager script
+        public FlockManager myManager;
+        // Prefab initial speed;
+        float speed;
+        // Bool used to check the swim limits
+        bool turning = false;
 
-        Vector3 vcentre = Vector3.zero;
-        Vector3 vavoid = Vector3.zero;
-        float gSpeed = 0.01f;
-        float nDistance;
-        int groupSize = 0;
-
-        foreach (GameObject go in gos) {
-
-            if (go != this.gameObject) {
-
-                nDistance = Vector3.Distance(go.transform.position, this.transform.position);
-                if (nDistance <= myManager.neighbourDistance) {
-
-                    vcentre += go.transform.position;
-                    groupSize++;
-
-                    if (nDistance < 1.0f) {
-
-                        vavoid = vavoid + (this.transform.position - go.transform.position);
-                    }
-
-                    Flock anotherFlock = go.GetComponent<Flock>();
-                    gSpeed = gSpeed + anotherFlock.speed;
-                }
-            }
+        void Start()
+        {
+            // Assign a random speed to each this prefab
+            speed = Random.Range(myManager.minSpeed, myManager.maxSpeed);
         }
 
-        if (groupSize > 0) {
+        // Update is called once per frame
+        void Update()
+        {
+            // Determine the bounding box of the manager cube
+            Bounds b = new Bounds(myManager.transform.position, myManager.swimLimits * 2.0f);
 
-            // Find the average centre of the group then add a vector to the target (goalPos)
-            vcentre = vcentre / groupSize + (myManager.goalPos - this.transform.position);
-            speed = gSpeed / groupSize;
+            // If the fish is outside the bounds of the cube or about to hit something
+            // then start turning around
+            RaycastHit hit = new RaycastHit();
+            Vector3 direction = Vector3.zero;
 
-            Vector3 direction = (vcentre + vavoid) - transform.position;
-            if (direction != Vector3.zero) {
+            if (!b.Contains(transform.position))
+            {
 
+                turning = true;
+                direction = myManager.transform.position - transform.position;
+            }
+            else if (Physics.Raycast(transform.position, this.transform.forward * 50.0f, out hit))
+            {
+                turning = true;
+                //Debug.DrawRay(this.transform.position, this.transform.forward * 50.0f, Color.red);
+                direction = Vector3.Reflect(this.transform.forward, hit.normal);
+            }
+            else
+            {
+
+                turning = false;
+            }
+
+            // Test if we're turning
+            if (turning)
+            {
+
+                // Turn towards the centre of the cube
                 transform.rotation = Quaternion.Slerp(transform.rotation,
                                                       Quaternion.LookRotation(direction),
                                                       myManager.rotationSpeed * Time.deltaTime);
+            }
+            else
+            {
+
+                // 10% chance of altering prefab speed
+                if (Random.Range(0.0f, 100.0f) < 10.0f)
+                {
+
+                    speed = Random.Range(myManager.minSpeed, myManager.maxSpeed);
+                }
+
+                // 20& chance of applying the flocking rules
+                if (Random.Range(0.0f, 100.0f) < 20.0f)
+                {
+
+                    ApplyRules();
+                }
+            }
+
+            transform.Translate(0.0f, 0.0f, Time.deltaTime * speed);
+        }
+
+        void ApplyRules()
+        {
+
+            GameObject[] gos;
+            gos = myManager.allFish;
+
+            Vector3 vcentre = Vector3.zero;
+            Vector3 vavoid = Vector3.zero;
+            float gSpeed = 0.01f;
+            float nDistance;
+            int groupSize = 0;
+
+            foreach (GameObject go in gos)
+            {
+
+                if (go != this.gameObject)
+                {
+
+                    nDistance = Vector3.Distance(go.transform.position, this.transform.position);
+                    if (nDistance <= myManager.neighbourDistance)
+                    {
+
+                        vcentre += go.transform.position;
+                        groupSize++;
+
+                        if (nDistance < 1.0f)
+                        {
+
+                            vavoid = vavoid + (this.transform.position - go.transform.position);
+                        }
+
+                        Flock anotherFlock = go.GetComponent<Flock>();
+                        gSpeed = gSpeed + anotherFlock.speed;
+                    }
+                }
+            }
+
+            if (groupSize > 0)
+            {
+
+                // Find the average centre of the group then add a vector to the target (goalPos)
+                vcentre = vcentre / groupSize + (myManager.goalPos - this.transform.position);
+                speed = gSpeed / groupSize;
+
+                Vector3 direction = (vcentre + vavoid) - transform.position;
+                if (direction != Vector3.zero)
+                {
+
+                    transform.rotation = Quaternion.Slerp(transform.rotation,
+                                                          Quaternion.LookRotation(direction),
+                                                          myManager.rotationSpeed * Time.deltaTime);
+                }
             }
         }
     }
--- Assets/Flocking/Scripts/FlockManager.cs
+++ Assets/Flocking/Scripts/FlockManager.cs
@@ -1,56 +1,61 @@
 ﻿using UnityEngine;
 
-public class FlockManager : MonoBehaviour
+namespace flocking
 {
-
-    // Access the fish prefab
-    public GameObject fishPrefab;
-    // Starting number of fish
-    public int numFish = 20;
-    // Array of fish prefabs
-    public GameObject[] allFish;
-    // Swimming bounds for fish
-    public Vector3 swimLimits = new Vector3(5.0f, 5.0f, 5.0f);
-    // Goal position
-    public Vector3 goalPos;
-
-    // Header title for Unity Editor
-    [Header("Fish Settings")]
-    [Range(0.0f, 5.0f)]
-    public float minSpeed;          // Minimum speed range
-    [Range(0.0f, 5.0f)]
-    public float maxSpeed;          // Maximum speed range
-    [Range(1.0f, 10.0f)]
-    public float neighbourDistance; // Prefab neighbout distance
-    [Range(0.0f, 5.0f)]
-    public float rotationSpeed;     // Speed at which the prefabs rotate
-
-    void Start()
+    public class FlockManager : MonoBehaviour
     {
-        // Allocate the allFish array
-        allFish = new GameObject[numFish];
-        // Loop throught the array instantiating the prefabs.  In this case fish
-        for (int i = 0; i < numFish; ++i) {
 
-            Vector3 pos = this.transform.position + new Vector3(Random.Range(-swimLimits.x, swimLimits.x),
-                                                                Random.Range(-swimLimits.x, swimLimits.x),
-                                                                Random.Range(-swimLimits.x, swimLimits.x));
-            allFish[i] = Instantiate(fishPrefab, pos, Quaternion.identity);
-            allFish[i].GetComponent<Flock>().myManager = this;
+        // Access the fish prefab
+        public GameObject fishPrefab;
+        // Starting number of fish
+        public int numFish = 20;
+        // Array of fish prefabs
+        public GameObject[] allFish;
+        // Swimming bounds for fish
+        public Vector3 swimLimits = new Vector3(5.0f, 5.0f, 5.0f);
+        // Goal position
+        public Vector3 goalPos;
+
+        // Header title for Unity Editor
+        [Header("Fish Settings")]
+        [Range(0.0f, 5.0f)]
+        public float minSpeed;          // Minimum speed range
+        [Range(0.0f, 5.0f)]
+        public float maxSpeed;          // Maximum speed range
+        [Range(1.0f, 10.0f)]
+        public float neighbourDistance; // Prefab neighbout distance
+        [Range(0.0f, 5.0f)]
+        public float rotationSpeed;     // Speed at which the prefabs rotate
+
+        void Start()
+        {
+            // Allocate the allFish array
+            allFish = new GameObject[numFish];
+            // Loop throught the array instantiating the prefabs.  In this case fish
+            for (int i = 0; i < numFish; ++i)
+            {
+
+                Vector3 pos = this.transform.position + new Vector3(Random.Range(-swimLimits.x, swimLimits.x),
+                                                                    Random.Range(-swimLimits.x, swimLimits.x),
+                                                                    Random.Range(-swimLimits.x, swimLimits.x));
+                allFish[i] = Instantiate(fishPrefab, pos, Quaternion.identity);
+                allFish[i].GetComponent<Flock>().myManager = this;
+            }
+
+            // Target for the prefbas to head for
+            goalPos = this.transform.position;
         }
 
-        // Target for the prefbas to head for
-        goalPos = this.transform.position;
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        // Update the target for the prefabs to head for with a random chance
-        if (Random.Range(0.0f, 100.0f) < 10.0f) {
-            goalPos = this.transform.position + new Vector3(Random.Range(-swimLimits.x, swimLimits.x),
-                                                            Random.Range(-swimLimits.x, swimLimits.x),
-                                                            Random.Range(-swimLimits.x, swimLimits.x));
+        // Update is called once per frame
+        void Update()
+        {
+            // Update the target for the prefabs to head for with a random chance
+            if (Random.Range(0.0f, 100.0f) < 10.0f)
+            {
+                goalPos = this.transform.position + new Vector3(Random.Range(-swimLimits.x, swimLimits.x),
+                                                                Random.Range(-swimLimits.x, swimLimits.x),
+                                                                Random.Range(-swimLimits.x, swimLimits.x));
+            }
         }
     }
 }
